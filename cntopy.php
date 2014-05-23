0fe087c29c2e36c18966e0e929733501d9ef600f
wangbo6 <wangbo6@360.cn>
2014-05-23 19:00:39 +0800

    修复不是汉字的处理

--- cntopy.php
+++ cntopy.php
@@ -1,9 +1,10 @@
 <?php
 function transform($text){
     $start = 19968;
+    $max = 125416;
     //note 获取字典文件
     $shm_key = ftok(__FILE__, 'a');
-    $shm_id = shmop_open($shm_key, "c", 0666, 125416);
+    $shm_id = shmop_open($shm_key, "c", 0666, $max);
     $data = shmop_read($shm_id, 0, 1);
     if($data[0] != 'y'){
         $data = file_get_contents("py.txt");
@@ -13,12 +14,19 @@ function transform($text){
         return false;
     }
     $str = json_encode($text);
+    echo $str;
     preg_match_all('/\\\\u([0-f]{4})/', $str, $match);
     foreach ($match[1] as $one){
         $tmp = hexdec($one);
         $offset = ($tmp - $start)*6;
-        $res = shmop_read($shm_id, $offset, 6);
-        $str = str_replace('\u' . $one, trim($res), $str);
+        if($offset>0 && $offset <= ($max-6)){
+            $res = shmop_read($shm_id, $offset, 6);
+        }else{
+            $res = "";
+        }
+        if($res){
+            $str = str_replace('\u' . $one, trim($res), $str);
+        }
     }
     return json_decode($str);
 }
